e1dded6a2a8d413e66acb0189d421ecb82f90011
vignesh-g-05 <vicky2805vky@gmail.com>
2026-03-10 00:15:03 +0530

    feat: create header and nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Hero from "./features/hero";
+import Header from "./features/header";
 
 export default function App() {
   return (
     <main className="mx-auto flex max-w-275 flex-col gap-20 px-5 py-10 pt-20 md:px-8 lg:px-10">
+      <Header />
       <Hero />
     </main>
   );
---
+++ src/features/header/index.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+import { AnimatedThemeToggler } from "@/components/ui/shadcn/animated-theme-toggler";
+
+const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header className="fixed top-0 right-0 left-0 z-50 bg-transparent px-6 py-3 backdrop-blur-3xl">
+      <div className="mx-auto flex max-w-275 items-center justify-between px-5 md:px-8 lg:px-10">
+        <p className="bg-primary text-background font-heading grid size-8 place-content-center rounded-sm font-bold">
+          V
+        </p>
+
+        <nav className="hidden md:block">
+          <ul className="flex items-center gap-6 text-sm font-medium">
+            <li className="cursor-pointer">About</li>
+            <li className="cursor-pointer">Skills</li>
+            <li className="cursor-pointer">Projects</li>
+            <li className="cursor-pointer">Experience</li>
+            <li className="cursor-pointer">Contact</li>
+          </ul>
+        </nav>
+
+        {/* Action + Mobile Hamburger */}
+        <div className="flex items-center gap-3">
+          {/* Theme Button */}
+          <AnimatedThemeToggler className="bg-primary text-primary-foreground grid size-8 place-content-center rounded-sm [&>svg]:size-4" />
+
+          <button
+            onClick={() => setOpen(!open)}
+            className="bg-primary text-primary-foreground grid size-8 place-content-center rounded-sm md:hidden [&>svg]:size-4"
+          >
+            {open ? <X /> : <Menu />}
+          </button>
+        </div>
+      </div>
+      {open && (
+        <nav className="bg-card animate-in slide-in-from-top-4 border-border fixed top-full right-6 z-40 mt-2 w-56 overflow-hidden rounded-md border shadow-lg md:hidden">
+          <ul className="text-sm font-medium">
+            <li className="hover:bg-secondary cursor-pointer px-4 py-2.5 transition-colors duration-200">
+              About
+            </li>
+            <li className="hover:bg-secondary cursor-pointer px-4 py-2.5 transition-colors duration-200">
+              Skills
+            </li>
+            <li className="hover:bg-secondary cursor-pointer px-4 py-2.5 transition-colors duration-200">
+              Projects
+            </li>
+            <li className="hover:bg-secondary cursor-pointer px-4 py-2.5 transition-colors duration-200">
+              Experience
+            </li>
+            <li className="hover:bg-secondary cursor-pointer px-4 py-2.5 transition-colors duration-200">
+              Contact
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
+  );
+};
+
+export default Header;
